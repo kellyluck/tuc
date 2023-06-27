fa0a648c758e773982374352a95f519bb289ff19
Kelly Luck <toymaker@usa.net>
2023-06-27 01:25:53 -0500

    added some capacity measurements

--- tucCapacity.js
+++ tucCapacity.js
@@ -2,8 +2,16 @@
 let tucCapacity = `720k floppy,720000,
 CD (74 min),650000000,
 Blu-Ray,25000000000,per layer
+Bible (King James version),4459453,
+Shakespeare (Complete Works),5236298,
+SMS message,160,
+Twitter (original),140,
+Twitter (current),280,
+Byte,1,
+Kilobyte,1000,
+Megabyte,1000000,
+Gigabyte,1000000000,
+Terabyte,1000000000000,
+Petabyte,1000000000000000,
+A La Recherche du Temps Perdu,9609000,by Marcel Proust; Guinness record holder for longest novel
 DVD,4700000000`;
-
-/* todo:
-    Dictionary,
-    King James Bible, */
